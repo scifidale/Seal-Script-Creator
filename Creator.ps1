ee0873165aa1cdc3814d4f9fcd86c4d1b4a2d059
scifidale <49734424+scifidale@users.noreply.github.com>
2019-09-11 14:09:09 +0100

    Systrack changes
    Added systrack script block

--- Creator.ps1
+++ Creator.ps1
@@ -7,12 +7,39 @@ set test="C:\bdlog.txt"
 Set FSLOGIX="c:\Program Files\FSLigix\Apps\frx.exe"
 Set Ivanti="C:\Program Files\AppSense\environment Manager\Agent\ENUser.exe"
 Set VMware="C:\Program Files\VMware\VMware Tools\vmtoolsd.exe"
+Set Systrack="C:\Program Files (x86)\Systrack\LsiAgent\LSiAgent.exe"
+
 
 REM ##### Create bare Seal Script Folder ####
 MD C:\Seal 
 Copy NUL > c:\Seal\SealScript.PS1
-
+REM ##### Citrix Gneralisation Phase #####
 Echo "EEC Services Seal Script" >> C:\Seal\sealscript.ps1
 
 If exist %test% (echo hellow >> "c:\UserGuidePDF\test.txt") Else ( REM File doesnt exist )
+
+
+REM ##### Scan for Systrack Agent #####
+if Exist %Systrack% (
+Echo "Net Stop LsiAgent"
+Echo "REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v SystemName /f"
+Echo "REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v SystemName /t REG_SZ"
+Echo "REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v MasterSystem /f"
+Echo "REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v MasterSystem /t REG_SZ"
+Echo "REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\LsiAgent\Settings" /f"
+Echo "REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\LsiAgent\Settings""
+Echo "del "D:\SystrackDB\*.*" /q /s"
+Echo "RMDIR "D:\SystrackDB\" /s /q"
+Echo "MKDIR "D:\SystrackDB\""
+) >> C:\Seal\SealScript.ps1
+
+
+REM ##### insert general seal up script options #####
+
+Echo "powercfg.exe /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c" >> c:\Seal\Sealscript.ps1
+Echo "powershell.exe -noprofile -executionpolicy bypass -command "wevtutil el | Foreach-Object {wevtutil cl "$_"}"" >> c:\Seal\SealScript.ps1
+Echo "##### Pagefile settings #####" >> c:\Seal\SealScript
+Echo "wmic pagefileset where name="C:\\pagefile.sys" delete" >> c:\Seal\SealScript
+Echo "wmic pagefileset create name="D:\pagefile.sys"" >> c:\Seal\SealScript
+Echo "wmic pagefileset where name="D:\\pagefile.sys" set InitialSize=512,MaximumSize=8096" >> c:\Seal\SealScript
 	Pause
