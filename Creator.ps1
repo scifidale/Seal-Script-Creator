39a8da6cf3898b1b2b08312b7250f2ea1526ed7f
scifidale <49734424+scifidale@users.noreply.github.com>
2019-09-15 22:23:13 +0100

    Update Creator.ps1
    changed systrack code block corrected " ' comments

--- Creator.ps1
+++ Creator.ps1
@@ -6,13 +6,13 @@ REM Variable enteries
 $SealFolder = "C:\Seal"
 $SealFile = "SealScript.ps1"
 
-$Citrix = "C:\Program Files\Citrix\Virtual Desktop  Agent\VDA.exe"
-$CitrixPVS = "C:\Program Files\Citrix\Provisioning Services\StatusTray.exe"
+$Citrix = "C:\bdlog.txt"
+$CitrixPVS = "C:\bdlog.txt"
 $test = "C:\bdlog.txt"
-$FSLOGIX = "c:\Program Files\FSLigix\Apps\frx.exe"
-$Ivanti = "C:\Program Files\AppSense\environment Manager\Agent\ENUser.exe"
-$VMware = "C:\Program Files\VMware\VMware Tools\vmtoolsd.exe"
-$Systrack = "C:\Program Files (x86)\Systrack\LsiAgent\LSiAgent.exe"
+$FSLOGIX = "C:\bdlog.txt"
+$Ivanti = "C:\bdlog.txt"
+$VMware = "C:\bdlog.txt"
+$Systrack = "C:\bdlog.txt"
 
 REM OS version check
 $os = Get-CimInstance Win32_OperatingSystem | Select -expand Caption
@@ -24,44 +24,44 @@ New-Item -path $SealFolder -ItemType Directory
 New-Item -path $SealFolder\$SealFile
 
 
-REM ##### Citrix Gneralisation Phase #####
-Echo "EEC Services Seal Script" >> C:\Seal\sealscript.ps1
+REM ##### Citrix Generalisation Phase #####
+Echo "EEC Services Seal Script" >> $SealFolder\$SealFile
 
 
 
 REM ##### Citrix provisioning Services actions #####
 If (test-path "$CitrixPVS") {
-Echo "REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters /V DisableTaskOffload /t REG_DWORD /d 0x1 /f" >> $SealFolder\$SealFile
+Echo 'REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters /V DisableTaskOffload /t REG_DWORD /d 0x1 /f' >> $SealFolder\$SealFile
+Echo 'Del "C:\Program Data\Citrix\Provisioning Services\Log\*.* /F/Q"' >> $SealFolder\$SealFile
 }
 
 REM ##### Scan for Systrack Agent #####
-if Exist %Systrack% (
-Echo "Net Stop LsiAgent"
-Echo "REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v SystemName /f"
-Echo "REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v SystemName /t REG_SZ"
-Echo "REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v MasterSystem /f"
-Echo "REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v MasterSystem /t REG_SZ"
-Echo "REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\LsiAgent\Settings" /f"
-Echo "REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\LsiAgent\Settings""
-Echo "del "D:\SystrackDB\*.*" /q /s"
-Echo "RMDIR "D:\SystrackDB\" /s /q"
-Echo "MKDIR "D:\SystrackDB\""
-) >> C:\Seal\SealScript.ps1
+if (test-path "$Systrack") {
+Echo "Net Stop LsiAgent" >> $SealFolder\$SealFile
+Echo 'REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v SystemName /f' >> $SealFolder\$SealFile
+Echo 'REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v SystemName /t REG_SZ' >> $SealFolder\$SealFile
+Echo 'REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v MasterSystem /f' >> $SealFolder\$SealFile
+Echo 'REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\Deploy" /v MasterSystem /t REG_SZ' >> $SealFolder\$SealFile
+Echo 'REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\LsiAgent\Settings" /f' >> $SealFolder\$SealFile
+Echo 'REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Lakeside Software\LsiAgent\Settings"' >> $SealFolder\$SealFile
+Echo 'del D:\SystrackDB\*.* /q /s' >> $SealFolder\$SealFile
+Echo 'RMDIR D:\SystrackDB\ /s /q' >> $SealFolder\$SealFile
+Echo 'MKDIR D:\SystrackDB\' >> $SealFolder\$SealFile
+ }
 
 
 REM ##### VMware Tools cleanup #####
-IF EXIST %VMware% (Echo "RD "C:\Programdata\Microsoft\Windows\start Menu\Programs\VMware" /S /Q"
-) >> C:\Seal\SealScript.PS1
+IF (Test-Path "$VMware") {Echo "RD "C:\Programdata\Microsoft\Windows\start Menu\Programs\VMware" /S /Q" >> $SealFolder\$SealFile }
 
 
 REM ##### insert general seal up script options #####
 
-Echo "powercfg.exe /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c" >> c:\Seal\Sealscript.ps1
-Echo "powershell.exe -noprofile -executionpolicy bypass -command "wevtutil el | Foreach-Object {wevtutil cl "$_"}"" >> $Sealloc
-Echo "##### Pagefile settings #####" >> c:\Seal\SealScript
-Echo "wmic pagefileset where name="C:\\pagefile.sys" delete" >> c:\Seal\SealScript
-Echo "wmic pagefileset create name="D:\pagefile.sys"" >> c:\Seal\SealScript
-Echo "wmic pagefileset where name="D:\\pagefile.sys" set InitialSize=512,MaximumSize=8096" >> c:\Seal\SealScript
+Echo 'powercfg.exe /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c' >> $SealFolder\$SealFile
+Echo "powershell.exe -noprofile -executionpolicy bypass -command "wevtutil el | Foreach-Object {wevtutil cl "$_"}"" >> $SealFolder\$SealFile
+Echo '##### Pagefile settings #####' >> $SealFolder\$SealFile
+Echo 'wmic pagefileset where name="C:\\pagefile.sys" delete' >> $SealFolder\$SealFile
+Echo 'wmic pagefileset create name="D:\pagefile.sys"' >> $SealFolder\$SealFile
+Echo 'wmic pagefileset where name="D:\\pagefile.sys" set InitialSize=512,MaximumSize=8096' >> $SealFolder\$SealFile
 	
 ##### OS Specific Generalisations for Server 2016 #####
 :Server2016
